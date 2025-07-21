d3335d4390aeb8a4469c3b2d4dbad41fbda488c6
lucas-ipsum <s0572357@htw-berlin.de>
2025-07-21 12:15:32 +0200

    updated server js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('URL', 'https://strapi-cms.schrum.xyz'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
